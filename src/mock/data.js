69b75f2e281e06d14fe24c4f351ec964b2a8610d
SamuelChia <funwithes6@gmail.com>
2018-06-13 17:38:32 +0800

    faet: 分离数据

---
+++ src/mock/data.js
@@ -0,0 +1,147 @@
+const departmentList = [
+  {
+    departmentId: 573765368832000,
+    name: "XX科技",
+    parentId: 0,
+    description: "",
+    isDeleted: 0,
+    created: 0,
+    lastmod: "2018-06-07 19:44:40"
+  },
+  {
+    departmentId: 574708097400832,
+    name: "人事",
+    parentId: 573765368832000,
+    description: "",
+    isDeleted: 0,
+    created: 0,
+    lastmod: "2018-06-08 11:21:11"
+  },
+  {
+    departmentId: 574708164509696,
+    name: "前端",
+    parentId: 573765368832000,
+    description: "",
+    isDeleted: 0,
+    created: 0,
+    lastmod: "2018-06-08 11:21:15"
+  },
+  {
+    departmentId: 574708231618560,
+    name: "运维",
+    parentId: 573765368832000,
+    description: "",
+    isDeleted: 0,
+    created: 0,
+    lastmod: "2018-06-08 11:21:19"
+  },
+  {
+    departmentId: 574708315504640,
+    name: "测试",
+    parentId: 573765368832000,
+    description: "",
+    isDeleted: 0,
+    created: 0,
+    lastmod: "2018-06-08 11:21:24"
+  },
+  {
+    departmentId: 574708432945152,
+    name: "后端",
+    parentId: 573765368832000,
+    description: "",
+    isDeleted: 0,
+    created: 0,
+    lastmod: "2018-06-08 11:21:31"
+  },
+  {
+    departmentId: 574708567162880,
+    name: "移动端",
+    parentId: 573765368832000,
+    description: "",
+    isDeleted: 0,
+    created: 0,
+    lastmod: "2018-06-08 11:21:39"
+  },
+  {
+    departmentId: 574708835598336,
+    name: "笑嘻嘻",
+    parentId: 573765368832000,
+    description: "",
+    isDeleted: 0,
+    created: 0,
+    lastmod: "2018-06-08 14:43:58"
+  },
+  {
+    departmentId: 574709104033792,
+    name: "笑嘻嘻1",
+    parentId: 574708835598336,
+    description: "",
+    isDeleted: 0,
+    created: 0,
+    lastmod: "2018-06-08 11:22:11"
+  },
+  {
+    departmentId: 574709154365440,
+    name: "笑嘻嘻2",
+    parentId: 574708835598336,
+    description: "",
+    isDeleted: 0,
+    created: 0,
+    lastmod: "2018-06-08 11:22:14"
+  },
+  {
+    departmentId: 574709204697088,
+    name: "笑嘻嘻3",
+    parentId: 574708835598336,
+    description: "",
+    isDeleted: 0,
+    created: 0,
+    lastmod: "2018-06-08 11:22:17"
+  },
+  {
+    departmentId: 574709322137600,
+    name: "笑嘻嘻2-1",
+    parentId: 574709154365440,
+    description: "",
+    isDeleted: 0,
+    created: 0,
+    lastmod: "2018-06-08 11:22:24"
+  },
+  {
+    departmentId: 574709372469248,
+    name: "笑嘻嘻2-2",
+    parentId: 574709154365440,
+    description: "",
+    isDeleted: 0,
+    created: 0,
+    lastmod: "2018-06-08 11:22:27"
+  },
+  {
+    departmentId: 574709422800896,
+    name: "笑嘻嘻2-3",
+    parentId: 574709154365440,
+    description: "",
+    isDeleted: 0,
+    created: 0,
+    lastmod: "2018-06-08 11:22:30"
+  },
+  {
+    departmentId: 574919322550272,
+    name: "笑嘻嘻4",
+    parentId: 574708835598336,
+    description: "",
+    isDeleted: 0,
+    created: 0,
+    lastmod: "2018-06-08 14:51:01"
+  },
+  {
+    departmentId: 574959252324352,
+    name: "嘛麦皮",
+    parentId: 574709204697088,
+    description: "",
+    isDeleted: 0,
+    created: 0,
+    lastmod: "2018-06-08 15:30:41"
+  }
+];
+export default departmentList;
